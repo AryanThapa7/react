18509491b0d86ee5b3284118e2d15f736da58bf0
Aryanthapa7 <aryanarthapa@gmail.com>
2026-09-02 09:41:16 +0545

    Update dashboard navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        DataAnalytics
+        DataAnalytics Dashboard
       </div>
 
       <div className="navbar-links">
